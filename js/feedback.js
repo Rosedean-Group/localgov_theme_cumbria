33ed4f135ad3e9a68471e431831dbecb5c35b29d
Dan Champion <support@championinternet.com>
2021-07-12 18:32:14 +0100

    Add feedback form

---
+++ js/feedback.js
@@ -0,0 +1,36 @@
+/**
+ * @file JS file for the header component.
+ */
+
+(function feedbackScript(Drupal) {
+  Drupal.behaviors.feedback = {
+    attach(context) {
+      context = context || document;
+      const feedbackButtons = context.querySelectorAll('.js-toggle-feedback');
+      if (!feedbackButtons.length) {
+        alert('Feck');
+        return;
+      }
+
+      feedbackButtons.forEach(feedbackButton => {
+        if (!feedbackButton.classList.contains('js-processed')) {
+
+          feedbackButton.addEventListener('click', function (event) {
+            const feedbackParent = this.closest('.js-toggle-container');
+            const toggleTarget = context.querySelector('#' + this.getAttribute('aria-controls'));
+            const currentState = this.getAttribute('aria-expanded');
+            if (currentState === 'false') {
+              this.setAttribute('aria-expanded', 'true');
+            } else {
+              this.setAttribute('aria-expanded', 'false');
+            }
+            feedbackParent.classList.toggle('inactive');
+            toggleTarget.classList.toggle('inactive');
+            event.preventDefault();
+          })
+          feedbackButton.classList.add('js-processed');
+        }
+      });
+    }
+  };
+}(Drupal));
